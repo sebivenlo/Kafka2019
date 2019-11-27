e17c59399ace75750ce1ade251a9e99dcbc0d9dd
iSultanis <i.sultanis@student.fontys.nl>
2019-11-27 11:13:43 +0100

    Add listener to show incoming message

--- ArticleBackend/src/main/java/Controller/ArticleController.java
+++ ArticleBackend/src/main/java/Controller/ArticleController.java
@@ -1,7 +1,10 @@
 package Controller;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +20,14 @@ public class ArticleController {
         this.kafkaTemplate = kafkaTemplate;
     }
 
+    @CrossOrigin
     @PostMapping(value = "/Article")
     public void postArticle(@RequestBody KafkaMessage kafkaMessage) {
         kafkaTemplate.send(topic, kafkaMessage.toString());
+    }
+
+    @KafkaListener(topics = "Article")
+    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+        System.out.println(cr.toString());
     }
 }
